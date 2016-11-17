aed2edc7a4e767edaf8112b3884f5d447f066925
Pascal Asselin <pascalasselin@hotmail.com>
2016-11-17 00:13:30 -0500

    test commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var restify = require('restify');
+var botbuilder = require('botbuilder');
+
+// // Creating bot
+// var connector = new botbuilder.ChatConnector();
+// var bot = new botbuilder.UniversalBot(connector);
+// bot.dialog('/', function(session) {
+//     session.send("Hello world!");
+// });
+
+// // Creating server
+// var server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function(){
+//     console.log('listening...');
+// });
+// server.post('/api/messages', connector.listen());
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function(){
+    console.log('listening...');
+});
+server.get('/', function(req, res, next){
+    res.send('Hello World!');
+    next();
+});

1d7cdd72a136f75b61f206b0f2c0ee5f23c38787
Pascal Asselin <Pascal Asselin>
2016-11-17 17:12:46 -0500

    FirstBot

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 var restify = require('restify');
 var botbuilder = require('botbuilder');
 
-// // Creating bot
-// var connector = new botbuilder.ChatConnector();
-// var bot = new botbuilder.UniversalBot(connector);
-// bot.dialog('/', function(session) {
-//     session.send("Hello world!");
-// });
+// Creating bot
+var connector = new botbuilder.ChatConnector();
+var bot = new botbuilder.UniversalBot(connector);
+bot.dialog('/', function(session) {
+    session.send("Hello world!");
+});
 
-// // Creating server
-// var server = restify.createServer();
-// server.listen(process.env.port || process.env.PORT || 3978, function(){
-//     console.log('listening...');
-// });
-// server.post('/api/messages', connector.listen());
-
+// Creating server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
     console.log('listening...');
 });
-server.get('/', function(req, res, next){
-    res.send('Hello World!');
-    next();
-});
+server.post('/api/messages', connector.listen());
